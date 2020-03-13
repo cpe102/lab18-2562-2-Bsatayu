0794f697f396c1da7c4b11284a721f9db15c87bf
Bsatayu <satayu_s@cmu.ac.th>
2020-03-13 13:43:27 +0700

    lab18

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
@@ -8,10 +9,31 @@ struct Rect{
 double overlap(Rect,Rect);
 
 int main(){
+	Rect k1,k2;
 	cout << "Please input Rect 1 (x y w h): ";
+	cin >> k1.x>>k1.y>>k1.w>>k1.h;
 	cout << "Please input Rect 2 (x y w h): ";
+	cin >> k2.x>>k2.y>>k2.w>>k2.h;
 	
 	
-	cout << "Overlap area = ";	
+	cout << "Overlap area = " << overlap(k1,k2);	
 	return 0;
+}
+
+double overlap(Rect A,Rect B){
+	double n;
+	double m;
+	double x1,x2,y1,y2;
+
+	x1 = A.x+A.w;
+	x2 = B.x+B.w;
+	y1 = A.y-A.h;
+	y2 = B.y-B.h;
+
+    n= abs(max(A.x,B.x)+min(x1,x2));
+    m= abs(min(A.y,B.y)-abs(max(y1,y2)));
+
+   int j = n*m;
+     
+	 return j;
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,1 +1,40 @@
 //Modify function overlap() form lab18_1.cpp by using pointers as input arguments.
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+struct Rect{
+	double x,y,w,h;
+};
+
+double overlap(Rect *,Rect *);
+
+int main(){
+	Rect k1,k2;
+	cout << "Please input Rect 1 (x y w h): ";
+	cin >> k1.x>>k1.y>>k1.w>>k1.h;
+	cout << "Please input Rect 2 (x y w h): ";
+	cin >> k2.x>>k2.y>>k2.w>>k2.h;
+	
+	
+	cout << "Overlap area = " << overlap(&k1,&k2);	
+	return 0;
+}
+
+double overlap(Rect *A,Rect *B){
+	double n;
+	double m;
+	double x1,x2,y1,y2;
+
+	x1 = A->x+A->w;
+	x2 = B->x+B->w;
+	y1 = A->y-B->h;
+	y2 = B->y-B->h;
+
+    n= abs(max(A->x,B->x)+abs(min(x1,x2)));
+    m= abs(min(A->y,B->y)-abs(max(y1,y2)));
+
+   int j = n*m;
+     
+	 return j;
+}
